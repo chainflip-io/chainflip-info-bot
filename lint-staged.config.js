1bcb6df5551e2879493eabd174e3bb2531edee1b
Andrew Dibble <andrew@chainflip.io>
2024-10-21 17:33:10 +0200

    add check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,6 @@ export default {
     'tsc -p tsconfig.json --noEmit',
     `prettier --check ${filenames.join(' ')}`,
     `eslint --max-warnings 0 --no-warn-ignored ${filenames.join(' ')}`,
+    'vitest run',
   ],
 };
