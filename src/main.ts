f59f956a8c72c21f0d6af20be4d8169d0e15f28a
Andrew Dibble <andrew@chainflip.io>
2024-10-23 17:41:47 +0200

    feat: configure bullmq

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-/* eslint-disable no-console */
-import { sleep } from '@chainflip/utils/async';
+import 'dotenv/config';
+import { initialize } from './queues/initialize.js';
 
 export const start = async () => {
-  let exit = false as boolean;
-
-  console.log('running');
-
-  process.once('SIGINT', () => {
-    console.log('SIGINT received');
-    exit = true;
-  });
-
-  process.once('SIGTERM', () => {
-    console.log('SIGTERM received');
-    exit = true;
-  });
-
-  while (!exit) {
-    await sleep(1000);
-  }
+  await initialize();
 };
 
 await start();
