a1ee0102af0caab435eacca96abdcabdede4c825
Andrew Dibble <andrew@chainflip.io>
2024-10-25 11:32:36 +0200

    add mocks

---
+++ src/__mocks__/env.ts
@@ -0,0 +1,6 @@
+export default {
+  REDIS_URL: 'redis://localhost:6379',
+  HTTP_SERVER_PORT: '8080',
+  DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1234567890/abcdefg',
+  TELEGRAM_BOT_TOKEN: '1234',
+};
