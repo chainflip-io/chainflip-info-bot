61f4cd32b446c2f02e458b09d0398628f0e403ae
Andrew Dibble <andrew@chainflip.io>
2024-10-25 15:35:19 +0200

    add assertions

--- src/queries/__tests__/swapVolume.test.ts
+++ src/queries/__tests__/swapVolume.test.ts
@@ -3,12 +3,14 @@ import request from 'graphql-request';
 import getSwapVolumeStats from '../swapVolume.js';
 import swapVolumeStats from './swapVolumeStats.json' with { type: 'json' };
 import lpFeeStats from './lpFeeStats.json' with { type: 'json' };
+import env from '../../env.js';
 
 describe('swapVolume', () => {
   it('gets the swap volume after a timestamp', async () => {
     vi.mocked(request).mockResolvedValueOnce(swapVolumeStats).mockResolvedValueOnce(lpFeeStats);
 
-    expect(await getSwapVolumeStats('2024-10-25T00:00:00Z')).toMatchInlineSnapshot(`
+    const after = '2024-10-25T00:00:00Z';
+    expect(await getSwapVolumeStats(after)).toMatchInlineSnapshot(`
       {
         "flipBurned": "2,343.00",
         "lpFees": "$1,516.62",
@@ -16,5 +18,10 @@ describe('swapVolume', () => {
         "swapVolume": "$3,029,020.71",
       }
     `);
+
+    expect(request).toHaveBeenCalledWith(env.EXPLORER_GATEWAY_URL, expect.anything(), {
+      after: after,
+    });
+    expect(request).toHaveBeenCalledWith(env.LP_GATEWAY_URL, expect.anything(), { after: after });
   });
 });
