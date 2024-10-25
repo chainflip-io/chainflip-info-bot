ec72f418200f99c5736fd65a19b11e0f5f1bebe7
Andrew Dibble <andrew@chainflip.io>
2024-10-25 11:57:29 +0200

    refactor: accept tokens as parameter (#10)

--- src/channels/discord.ts
+++ src/channels/discord.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import env from '../env.js';
 
-export const sendMessage = async (content: string) => {
-  const response = await axios.post(env.DISCORD_WEBHOOK_URL, { content });
+export const sendMessage = async (webhookUrl: string, content: string) => {
+  const response = await axios.post(webhookUrl, { content });
 
   if (!(response.data as { ok: boolean }).ok) {
     throw new Error(`Failed to send message to discord: ${JSON.stringify(response.data)}`);
--- src/channels/telegram.ts
+++ src/channels/telegram.ts
@@ -1,10 +1,13 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import axios from 'axios';
 import React from 'react';
-import env from '../env.js';
 
-export const sendMessage = async (channel: string | number, text: React.JSX.Element) => {
-  const url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`;
+export const sendMessage = async (
+  token: string,
+  channel: string | number,
+  text: React.JSX.Element,
+) => {
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
   const response = await axios.post(url, {
     chat_id: channel,
