691a7fa753ea265ed05799b254444c98406c7fd5
Andrew Dibble <andrew@chainflip.io>
2024-10-25 11:36:51 +0200

    feat: add global setup

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['./src/__mocks__/setup.ts'],
     restoreMocks: true,
     include: ['**/*.test.ts', '**/*.test.tsx'],
     coverage: {
