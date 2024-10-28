76148fc445a8e8dbc7704d52121c9138763ce7c3
Andrew Dibble <andrew@dibble.codes>
2024-10-28 20:47:20 +0100

    fix config

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     setupFiles: ['./src/__mocks__/setup.ts'],
     restoreMocks: true,
-    include: ['./src/**/*.test.ts', './src/**/*.test.ts'],
+    include: ['./src/**/*.test.ts', './src/**/*.test.tsx'],
     coverage: {
       enabled: true,
       exclude: ['**/__tests__/**', './**/*.js', '**/__mocks__/**'],
