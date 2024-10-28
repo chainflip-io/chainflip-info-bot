03d7e9cc73ff68d24115ff106d36d74a0d21a918
Christina Gorokhovska <kristina@chainflip.io>
2024-10-29 00:29:18 +0100

    test: add tests for swaps, strings and date utils, change abbreviate method

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -3,7 +3,7 @@ export const abbreviate = (
   showLength = 4,
   space = false,
 ): string => {
-  if (typeof text !== 'string') return '';
+  if (typeof text !== 'string' || text.length === 0) return '';
   const leftPart = text.slice(0, showLength);
   const rightPart = text.slice(text.length - showLength);
 
