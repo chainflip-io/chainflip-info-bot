d6292ec49fad05b596d2366f6d95a4c196a93335
Andrew Dibble <andrew@chainflip.io>
2024-10-29 11:41:12 +0100

    change condition

--- src/channels/discord.ts
+++ src/channels/discord.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const sendMessage = async (webhookUrl: string, content: string) => {
   const response = await axios.post(webhookUrl, { content });
 
-  if (!(response.data as { ok: boolean }).ok) {
+  if (response.status !== 204) {
     throw new Error(`Failed to send message to discord: ${JSON.stringify(response.data)}`);
   }
 };
