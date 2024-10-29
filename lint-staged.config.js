091a979211329e98af248576ca2546cd91a47e6c
Andrew Dibble <andrew@chainflip.io>
2024-10-29 12:03:46 +0100

    chore: update eslint config (again)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,22 @@
-export default {
-  '**/*.ts': (filenames) => [
-    'tsc -p tsconfig.json --noEmit',
-    `prettier --check ${filenames.join(' ')}`,
-    `eslint --max-warnings 0 --no-warn-ignored ${filenames.join(' ')}`,
-    'vitest run',
-  ],
+export default (inputFilenames) => {
+  const filenames = inputFilenames.map((filename) => ({
+    projectPath: filename.replace(`${import.meta.dirname}/`, ''),
+    fullPath: filename,
+  }));
+
+  const commands = [`prettier --check ${inputFilenames.join(' ')}`];
+
+  if (inputFilenames.some((filename) => /\.tsx?$/.test(filename))) {
+    commands.push('tsc -p tsconfig.json --noEmit');
+  }
+
+  const srcFiles = filenames
+    .filter((filename) => filename.projectPath.startsWith('src'))
+    .map((filename) => filename.fullPath);
+
+  if (srcFiles.length) {
+    commands.push(`eslint --max-warnings 0 --no-warn-ignored ${srcFiles.join(' ')}`, 'vitest run');
+  }
+
+  return commands;
 };
