aa0dda4384f9ead65b666413e194ebbdc0d784c9
Andrew Dibble <andrew@chainflip.io>
2024-10-31 17:09:54 +0100

    remove property

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,9 +2,7 @@ import pino from 'pino';
 import env from '../env.js';
 
 const logger = pino.default(
-  env.NODE_ENV !== 'production'
-    ? { transport: { target: 'pino-pretty', options: { coloize: true } } }
-    : {},
+  env.NODE_ENV !== 'production' ? { transport: { target: 'pino-pretty' } } : {},
 );
 
 export default logger;
