4b102fe62e8fd41fc0fc594f89fc52540656805c
sukhoviianastasia <99375490+sukhoviianastasia@users.noreply.github.com>
2024-11-05 12:09:46 +0100

    feat: add twitter [WEB-1597] (#46)
    * fix(bot):added twitter & merge fix
    
    * fix(bot): removed twitter from config
    
    * fix(bot): minor fix
    
    * Update .gitignore
    
    Co-authored-by: jerryafr <hamedwise@gmail.com>
    
    * fix(bot): fixed test & naming
    
    * fix(bot):fixed to toMatchObject
    
    * add toHaveBeenCalledTimes
    
    ---------
    
    Co-authored-by: jerryafr <hamedwise@gmail.com>

---
+++ src/channels/__tests__/twitter.test.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import axios from 'axios';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { vi, describe, it, expect } from 'vitest';
+import { sendMessage } from '../twitter.js';
+
+describe('sendMessage', () => {
+  it('sends a message to the channel', async () => {
+    const postMock = vi.mocked(axios.post);
+
+    postMock.mockResolvedValue({
+      data: {
+        text: renderToStaticMarkup(
+          <>
+            hello twitter<strong>bold</strong>
+          </>,
+        ),
+        id: '1853357330332549327',
+        edit_history_tweet_ids: ['1853357330332549327'],
+      },
+    });
+
+    await sendMessage(
+      {
+        consumerKey: 'consumer_key',
+        consumerKeySecret: 'consumer_key_secret',
+        oauthKey: 'oauth_key',
+        oauthKeySecret: 'oauth_key_secret',
+      },
+      renderToStaticMarkup(
+        <>
+          hello twitter<strong>bold</strong>
+        </>,
+      ),
+    );
+
+    expect(postMock).toHaveBeenCalledTimes(1);
+
+    expect(postMock.mock.lastCall).toMatchObject([
+      'https://api.twitter.com/2/tweets',
+      {
+        text: 'hello twitter<strong>bold</strong>',
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: expect.stringMatching(
+            /OAuth oauth_consumer_key="consumer_key", oauth_nonce=".+?", oauth_signature=".+?", oauth_signature_method="PLAINTEXT", oauth_timestamp="\d+", oauth_token="oauth_key", oauth_version="1\.0"/,
+          ) as string,
+          'Content-Type': 'application/json',
+          'User-Agent': 'v2CreateTweetJS',
+        },
+      },
+    ]);
+  });
+
+  it('throws an error if the response is not ok', async () => {
+    const postMock = vi.mocked(axios.post);
+
+    postMock.mockResolvedValue({ data: { ok: false, description: 'some other stuff here' } });
+
+    await expect(
+      sendMessage(
+        {
+          consumerKey: 'consumer_key',
+          consumerKeySecret: 'consumer_key_secret',
+          oauthKey: 'oauth_key',
+          oauthKeySecret: 'oauth_key_secret',
+        },
+        renderToStaticMarkup(
+          <>
+            hello twitter<strong>bold</strong>
+          </>,
+        ),
+      ),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: failed to send message: {"ok":false,"description":"some other stuff here"}]`,
+    );
+  });
+});
---
+++ src/channels/twitter.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import crypto from 'crypto';
+import OAuth from 'oauth-1.0a';
+
+export type TwitterConfig = {
+  consumerKey: string;
+  consumerKeySecret: string;
+  oauthKey: string;
+  oauthKeySecret: string;
+};
+
+export const sendMessage = async (token: TwitterConfig, text: string) => {
+  const url = `https://api.twitter.com/2/tweets`;
+
+  const { consumerKey, consumerKeySecret, oauthKey, oauthKeySecret } = token;
+
+  const oauth = new OAuth({
+    consumer: {
+      key: consumerKey,
+      secret: consumerKeySecret,
+    },
+    hash_function: (baseString, key) =>
+      crypto.createHmac('sha1', key).update(baseString).digest('base64'),
+  });
+
+  const authHeader = oauth.toHeader(
+    oauth.authorize(
+      {
+        url,
+        method: 'POST',
+      },
+      { key: oauthKey, secret: oauthKeySecret },
+    ),
+  );
+
+  const response = await axios.post(
+    url,
+    { text },
+    {
+      headers: {
+        Authorization: authHeader['Authorization'],
+        'User-Agent': 'v2CreateTweetJS',
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    },
+  );
+
+  const formattedResponse = response.data as { id: string; text: string };
+  if (!formattedResponse.id || !formattedResponse.text) {
+    throw new Error(`failed to send message: ${JSON.stringify(response.data)}`);
+  }
+};
