351f22521e8b34295add1180ba2f4598a231ba38
Christina Gorokhovska <kristina@chainflip.io>
2024-11-08 09:11:03 +0100

    fix: change statements for swapStatusCheck and cleanup

--- src/queues/swapStatusCheck.tsx
+++ src/queues/swapStatusCheck.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { formatUsdValue } from '@chainflip/utils/number';
 import { type BigNumber } from 'bignumber.js';
+import { UnrecoverableError } from 'bullmq';
 import { differenceInMinutes } from 'date-fns';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { type DispatchJobArgs, type JobConfig, type JobProcessor } from './initialize.js';
@@ -203,24 +204,20 @@ const processJob: JobProcessor<Name> = (dispatchJobs) => async (job) => {
 
   const status = getSwapStatus(swapInfo);
 
-  switch (status) {
-    case 'fresh':
-      jobs.push(...buildMessageData({ swapInfo }));
-      logger.info(`Swap #${swapInfo.requestId} is fresh, job was added in a queue`);
-      break;
-
-    case 'stale':
-      logger.warn(`Swap #${swapInfo.requestId} is stale`);
-      break;
-
-    case 'pending':
-      jobs.push({
-        name: 'scheduler',
-        data: [{ name, data: { swapRequestId: job.data.swapRequestId } }],
-        opts: { delay: 10_000 },
-      } as const);
-      logger.info(`Swap #${swapInfo.requestId} is not completed, pushed to a scheduler`);
-      break;
+  if (status === 'fresh') {
+    jobs.push(...buildMessageData({ swapInfo }));
+    logger.info(`Swap #${swapInfo.requestId} is fresh, job was added in a queue`);
+  } else if (status === 'stale') {
+    logger.warn(`Swap #${swapInfo.requestId} is stale`);
+  } else if (status === 'pending') {
+    jobs.push({
+      name: 'scheduler',
+      data: [{ name, data: { swapRequestId: job.data.swapRequestId } }],
+      opts: { delay: 10_000 },
+    } as const);
+    logger.info(`Swap #${swapInfo.requestId} is not completed, pushed to a scheduler`);
+  } else {
+    throw new UnrecoverableError('Swap status in unpredictable');
   }
 
   await dispatchJobs(jobs);
