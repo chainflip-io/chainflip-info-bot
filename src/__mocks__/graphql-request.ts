f8a0fc3a84d483e5cd6f8a6e385106c463b3c466
Andrew Dibble <andrew@chainflip.io>
2024-11-08 18:29:49 +0100

    rename files

--- src/__mocks__/graphql-request.ts
+++ src/__mocks__/graphql-request.ts
@@ -1,7 +1,8 @@
+import { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import assert from 'assert';
 import { createHash } from 'crypto';
 import * as fs from 'fs/promises';
-import { type RequestDocument } from 'graphql-request';
+import { Kind } from 'graphql';
 import * as path from 'path';
 import { vi } from 'vitest';
 
@@ -13,7 +14,7 @@ vi.mock('graphql-request', (importActual) => {
       const mapped = {
         'https://chainflap-explor.org/graphql':
           'https://explorer-service-processor.chainflip.io/graphql',
-        'https://chainflap-lp.org/graphql': 'https://lp-service.mainnet.chainflip.io/',
+        'https://chainflap-lp.org/graphql': 'https://lp-service.mainnet.chainflip.io/graphql',
       }[url];
 
       assert(mapped, `Unknown URL: ${url}`);
@@ -21,7 +22,7 @@ vi.mock('graphql-request', (importActual) => {
       this.url = mapped;
     }
 
-    async request(query: RequestDocument, variables: Record<string, any>) {
+    async request(query: TypedDocumentNode<any, any>, variables: Record<string, any>) {
       // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
       const { default: request } = (await importActual()) as typeof import('graphql-request');
       const sha1 = (str: string) => createHash('sha1').update(str).digest('hex');
@@ -29,12 +30,18 @@ vi.mock('graphql-request', (importActual) => {
       const variablesHash = sha1(JSON.stringify(variables));
 
       const fixturesDir = path.join(import.meta.dirname, '__fixtures__');
-      const cachedFile = path.join(fixturesDir, queryHash, `${variablesHash}.json`);
+      // const name = query.definitions[0].
+      assert(query.kind === Kind.DOCUMENT, 'Expected query to be a Document');
+      const [def] = query.definitions;
+      assert(def.kind === Kind.OPERATION_DEFINITION, 'Expected query to be an OperationDefinition');
+      assert(def.name, 'Expected query to have a name');
+      const name = def.name.value;
+      const cachedFile = path.join(fixturesDir, `${name}-${queryHash}`, `${variablesHash}.json`);
       const file = await fs.readFile(cachedFile, 'utf8').catch(() => null);
 
       if (file) return JSON.parse(file) as unknown;
 
-      const result = await request(this.url, query, variables);
+      const result = (await request(this.url, query, variables)) as unknown;
 
       await fs.mkdir(path.dirname(cachedFile), { recursive: true });
       await fs.writeFile(cachedFile, JSON.stringify(result, null, 2), 'utf8');
