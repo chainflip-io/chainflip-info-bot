53043b7739a18315282f01d836774ccddc583a16
Andrew Dibble <andrew@chainflip.io>
2024-11-18 19:26:56 +0100

    refactor: remove count in pulse

--- src/utils/pulse.ts
+++ src/utils/pulse.ts
@@ -1,25 +1,13 @@
 export class Pulse {
   private last: number | null = null;
 
-  private checks = 0;
-
   beat() {
     this.last = performance.now();
   }
 
   check(): 'healthy' | 'dying' | 'dead' {
     // if beat is never called
-    if (this.last === null) {
-      // and we check three times
-      if (this.checks >= 3) {
-        // the worker has stalled
-        return 'dead';
-      }
-
-      // otherwise we assume it's starting up
-      this.checks += 1;
-      return 'healthy';
-    }
+    if (this.last === null) return 'dead';
 
     const diff = performance.now() - this.last;
     // if the last beat was less than 30 seconds ago, we gucci
